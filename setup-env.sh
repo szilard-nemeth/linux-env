a93bfc7289f536168971821517d2b280cd38f636
snemeth <nsziszy@gmail.com>
2016-07-11 12:25:03 +0200

    corrected alias copying command in setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -1,5 +1,7 @@
 #TODO add warning about overwrite before copying to home directory!
 
-yes | cp -aR ./aliases ~/aliases;
+yes | cp -aR ./aliases/. ~/aliases/
 yes | cp -aR ./dotfiles/. ~/
 cp .bashrc ~/.bashrc;
+
+source ~/.bashrc
