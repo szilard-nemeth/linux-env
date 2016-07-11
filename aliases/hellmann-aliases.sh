c3f6ba48a91c1f722a12b6bfb9d2f3c72ccaceaf
snemeth <nsziszy@gmail.com>
2016-07-11 14:58:15 +0200

    added alias to hellmann aliases

--- aliases/hellmann-aliases.sh
+++ aliases/hellmann-aliases.sh
@@ -1,3 +1,7 @@
+#GOTO aliases
 alias goto-tracking="cd /home/snemeth/development/hellmann-repos/trackingall"
 alias goto-rlr="cd /home/snemeth/development/hellmann-repos/rlr"
+
+#DOCKER aliases
+alias docker-tracking-start-int-env="~/development/hellmann-repos/trackingall/tracking-docker/tracking-docker-compose/tracking/call-compose.sh localhost local tracking_1 up"
 
