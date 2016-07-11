3043797cc6795b1afafabd12997300c34ac42520
snemeth <nsziszy@gmail.com>
2016-07-11 16:43:34 +0200

    improved hellmann aliases

--- aliases/hellmann-aliases.sh
+++ aliases/hellmann-aliases.sh
@@ -1,19 +1,17 @@
 #setup locations
-HELLMANN_DEV_ROOT="/home/snemeth/development/hellmann-repos/"
+HELLMANN_DEV_ROOT="$HOME/development/hellmann-repos/"
 TRACKING_ROOT="$HELLMANN_DEV_ROOT/trackingall/"
-RLR_ROOT="$HELLMANN_DEV_ROOT/rlr/"
 TRACKING_DOCKER_COMPOSE_ROOT="$TRACKING_ROOT/tracking-docker/tracking-docker-compose/tracking/"
+RLR_ROOT="$HELLMANN_DEV_ROOT/rlr/"
 RLR_DOCKER_COMPOSE_ROOT="$RLR_ROOT/rlr-docker-compose/rlr/"
 
-
 #goto aliases
-alias goto-tracking="cd /home/snemeth/development/hellmann-repos/trackingall"
-alias goto-rlr="cd /home/snemeth/development/hellmann-repos/rlr"
+alias goto-tracking="cd $HELLMANN_DEV_ROOT/trackingall"
+alias goto-rlr="cd $HELLMANN_DEV_ROOT/rlr"
 
 #maven aliases
-alias mvn-st="mvn clean install -DskipTests"
-alias mvn-skipall="mvn clean install -DskipTests -Dnogwt -Dnodocker"
-
+alias mvn-ci-skiptest="mvn clean install -DskipTests"
+alias mvn-ci-skip-all="mvn clean install -DskipTests -Dnogwt -Dnodocker"
 
 #docker aliases
 #Tracking
