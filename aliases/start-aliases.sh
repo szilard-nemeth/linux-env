5ba69f686c3aae1664cd2827ea636e8a8f21d6f8
snemeth <nsziszy@gmail.com>
2016-12-02 23:51:28 +0100

    changed intellij and hipchat start command

--- aliases/start-aliases.sh
+++ aliases/start-aliases.sh
@@ -1,3 +1,5 @@
 alias start-google-chrome="google-chrome > /dev/null 2>&1"
-alias start-intellij="/usr/local/bin/idea > /dev/null 2>&1"
+#alias start-intellij="/usr/local/bin/idea > /dev/null 2>&1"
+alias start-intellij="/usr/local/intellij-idea/bin/idea.sh"
 alias start-squirrel="/usr/local/squirrel/squirrel-sql.sh > /dev/null 2>&1"
+alias start-hipchat4="hipchat4 > /dev/null 2>&1"
