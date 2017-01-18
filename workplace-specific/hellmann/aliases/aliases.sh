5535c35360c23048e7cb22300db72c70a700bce9
nemethszyszy <nsziszy@gmail.com>
2017-01-18 17:13:50 +0100

    fixed hellmann docker aliases

--- workplace-specific/hellmann/aliases/aliases.sh
+++ workplace-specific/hellmann/aliases/aliases.sh
@@ -1,7 +1,11 @@
 #setup locations
+TRACKING_COMPOSE_NAME="tracking_1"
+TRACKING_COMPOSE_ARGS="localhost local $TRACKING_COMPOSE_NAME"
 HELLMANN_DEV_ROOT="$HOME/development/hellmann-repos/"
 TRACKING_ROOT="$HELLMANN_DEV_ROOT/trackingall/"
-TRACKING_DOCKER_COMPOSE_ROOT="$TRACKING_ROOT/tracking-docker/tracking-docker-compose/tracking/"
+TRACKING_DOCKER_COMPOSE_ROOT_TR="$TRACKING_ROOT/tracking-docker/tracking-docker-compose/tracking/"
+TRACKING_DOCKER_COMPOSE_ROOT_TDS="$TRACKING_ROOT/tracking-docker/tracking-docker-compose/dataservice/"
+TRACKING_DOCKER_COMPOSE_ROOT_KAFKA="$TRACKING_ROOT/tracking-docker/tracking-docker-compose/kafka-feeder/"
 RLR_ROOT="$HELLMANN_DEV_ROOT/rlr/"
 RLR_DOCKER_COMPOSE_ROOT="$RLR_ROOT/rlr-docker-compose/rlr/"
 
@@ -26,10 +30,21 @@ alias mvn-setversion=mavenSetVersion
 
 #docker aliases
 #Tracking
-alias docker-tracking-start-int-env="$TRACKING_ROOT/build/start-integration-env.sh"
-alias docker-tracking-start-env="$TRACKING_DOCKER_COMPOSE_ROOT/call-compose.sh localhost local tracking_1 up"
-alias docker-tracking-stop-env="$TRACKING_DOCKER_COMPOSE_ROOT/call-compose.sh localhost local tracking_1 kill && $TRACKING_DOCKER_COMPOSE_ROOT/call-compose.sh localhost local tracking_1 rm -fv"
-alias docker-tracking-logs="$TRACKING_DOCKER_COMPOSE_ROOT/call-compose.sh localhost local tracking_1 logs"
+alias docker-tracking-start-integration="$TRACKING_ROOT/build/start-integration-env.sh"
+alias docker-tracking-start="$TRACKING_DOCKER_COMPOSE_ROOT_TR/call-compose.sh $TRACKING_COMPOSE_ARGS up -d" \
+                            "$TRACKING_DOCKER_COMPOSE_ROOT_TDS/call-compose.sh $TRACKING_COMPOSE_ARGS up -d" \
+                            "$TRACKING_DOCKER_COMPOSE_ROOT_KAFKA/call-compose.sh $TRACKING_COMPOSE_ARGS up -d";
+
+alias docker-tracking-stop="$TRACKING_DOCKER_COMPOSE_ROOT_TR/call-compose.sh $TRACKING_COMPOSE_ARGS kill" \
+                            "$TRACKING_DOCKER_COMPOSE_ROOT_TDS/call-compose.sh $TRACKING_COMPOSE_ARGS kill" \
+                            "$TRACKING_DOCKER_COMPOSE_ROOT_KAFKA/call-compose.sh $TRACKING_COMPOSE_ARGS kill" \
+                            "$TRACKING_DOCKER_COMPOSE_ROOT_TR/call-compose.sh $TRACKING_COMPOSE_ARGS rm -fv" \
+                            "$TRACKING_DOCKER_COMPOSE_ROOT_TDS/call-compose.sh $TRACKING_COMPOSE_ARGS rm -fv" \
+                            "$TRACKING_DOCKER_COMPOSE_ROOT_KAFKA/call-compose.sh $TRACKING_COMPOSE_ARGS rm -fv";
+
+alias docker-tracking-logs="$TRACKING_DOCKER_COMPOSE_ROOT_TR/call-compose.sh $TRACKING_COMPOSE_ARGS logs"
+
+
 #RLR
 alias docker-rlr-start-int-env="$RLR_ROOT/build/start-db-env.sh"
 alias docker-rlr-start-env="$RLR_DOCKER_COMPOSE_ROOT/call-compose.sh localhost local rlr_1 up"
@@ -39,7 +54,7 @@ alias docker-rlr-logs="$RLR_DOCKER_COMPOSE_ROOT/call-compose.sh localhost local 
 #docker test
 #TODO add instance-name parameters
 #TODO add since parameter (last 24 hours!)
-alias docker-tracking-test-logs="$TRACKING_DOCKER_COMPOSE_ROOT/call-compose.sh docker@emea-jas-a12t.hwl-family.net test user-acceptance-b logs --follow"
+alias docker-tracking-test-logs="$TRACKING_DOCKER_COMPOSE_ROOT_TR/call-compose.sh docker@emea-jas-a12t.hwl-family.net test user-acceptance-b logs --follow"
 alias docker-rlr-test-logs="$RLR_DOCKER_COMPOSE_ROOT/call-compose.sh docker@emea-jas-a12t.hwl-family.net test rlr-test logs --follow"
 
 #other aliases
