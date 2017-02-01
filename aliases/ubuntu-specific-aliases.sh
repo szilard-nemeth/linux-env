e1b5d35168f32c8777ad3bc48ddb9740a6ca611b
snemeth <nsziszy@gmail.com>
2017-02-01 10:19:02 +0100

    commented old suspend alias

--- aliases/ubuntu-specific-aliases.sh
+++ aliases/ubuntu-specific-aliases.sh
@@ -1,4 +1,4 @@
 #TODO add alias: greps all TODOs in linux_env repo
 
 #TODO add alias which asks a question like "Really shutdown Y/N : 
-alias suspend='sudo pm-suspend'
+#alias suspend='sudo pm-suspend'
