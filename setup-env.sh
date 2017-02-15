75931984d3e0e3ebaeaff5feedd603640f28842a
snemeth <nsziszy@gmail.com>
2017-02-15 23:31:31 +0100

    store copied files from linux env repo into home directory

--- setup-env.sh
+++ setup-env.sh
@@ -1,25 +1,34 @@
 #TODO add warning about overwrite before copying to home directory!
 
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ENV_FILE_MAPPINGS=$HOME/.env/.file-mappings
+mkdir -p $HOME/.env
+rm ENV_FILE_MAPPINGS
+touch $ENV_FILE_MAPPINGS
+
 
 echo "Copying files from $DIR/aliases to $HOME/aliases"
 yes | cp -aR $DIR/aliases/. $HOME/aliases/
+echo "$DIR/aliases/ $HOME/aliases/" >> $ENV_FILE_MAPPINGS
 
 echo "Copying files from $DIR/dotfiles to $HOME"
 yes | cp -aR $DIR/dotfiles/. ~/
+echo "$DIR/dotfiles/ $HOME/" >> $ENV_FILE_MAPPINGS
 
 echo "Copying files from $DIR/scripts to $HOME/scripts"
 yes | cp -aR $DIR/scripts/. $HOME/scripts
+echo "$DIR/scripts/ $HOME/scripts" >> $ENV_FILE_MAPPINGS
 
 echo "Copying $DIR/.bashrc to $HOME/.bashrc"
 cp $DIR/.bashrc ~/.bashrc;
-
+echo "$DIR/.bashrc $HOME/.bashrc" >> $ENV_FILE_MAPPINGS
 
 SRC_DIR=$DIR/dotfiles/i3/
 DEST_DIR=$HOME/.i3/
 echo "Copying $SRC_DIR to $DEST_DIR"
 cp $SRC_DIR/* $DEST_DIR
+echo "$SRC_DIR/* $DEST_DIR" >> $ENV_FILE_MAPPINGS
 
 
 ##copy workplace-specific aliases
@@ -29,6 +38,7 @@ echo "Copying workplace-specific aliases..."
 echo "Copying $SRC_DIR to $HOME/"
 
 test -d "$DEST_DIR" || mkdir -p "$DEST_DIR" && yes | cp -aR $SRC_DIR $DEST_DIR
+echo "$SRC_DIR $DEST_DIR" >> $ENV_FILE_MAPPINGS
 
 echo Sourcing files from $HOME/aliases;
 for f in $HOME/aliases/*.sh; do
