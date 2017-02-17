54e5bfda03aa303457b032af193e2758c3ed316a
Szilard Nemeth <nsziszy@gmail.com>
2017-02-17 13:15:12 +0100

    improved setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -1,10 +1,11 @@
+#!/usr/bin/env bash
 #TODO add warning about overwrite before copying to home directory!
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ENV_FILE_MAPPINGS=$HOME/.env/.file-mappings
 mkdir -p $HOME/.env
-rm ENV_FILE_MAPPINGS
+rm $ENV_FILE_MAPPINGS
 touch $ENV_FILE_MAPPINGS
 
 
@@ -13,7 +14,7 @@ yes | cp -aR $DIR/aliases/. $HOME/aliases/
 echo "$DIR/aliases/ $HOME/aliases/" >> $ENV_FILE_MAPPINGS
 
 echo "Copying files from $DIR/dotfiles to $HOME"
-yes | cp -aR $DIR/dotfiles/. ~/
+yes | cp -aR $DIR/dotfiles/. $HOME/
 echo "$DIR/dotfiles/ $HOME/" >> $ENV_FILE_MAPPINGS
 
 echo "Copying files from $DIR/scripts to $HOME/scripts"
@@ -21,14 +22,14 @@ yes | cp -aR $DIR/scripts/. $HOME/scripts
 echo "$DIR/scripts/ $HOME/scripts" >> $ENV_FILE_MAPPINGS
 
 echo "Copying $DIR/.bashrc to $HOME/.bashrc"
-cp $DIR/.bashrc ~/.bashrc;
+cp $DIR/.bashrc $HOME/.bashrc;
 echo "$DIR/.bashrc $HOME/.bashrc" >> $ENV_FILE_MAPPINGS
 
 SRC_DIR=$DIR/dotfiles/i3/
 DEST_DIR=$HOME/.i3/
 echo "Copying $SRC_DIR to $DEST_DIR"
-cp $SRC_DIR/* $DEST_DIR
-echo "$SRC_DIR/* $DEST_DIR" >> $ENV_FILE_MAPPINGS
+yes | cp -aR $SRC_DIR/. $DEST_DIR
+echo "$SRC_DIR $DEST_DIR" >> $ENV_FILE_MAPPINGS
 
 
 ##copy workplace-specific aliases
@@ -47,19 +48,19 @@ for f in $HOME/aliases/*.sh; do
 done
 echo Done sourcing files from ~/aliases;
 
-echo "Searching for .add-to-path files and sourcing them..."
+echo "Searching for .add-to-path files and adding them to PATH..."
 matched_dirs=$(find $HOME/workplace-specific/ -name .add-to-path -printf "%h\n")
 for d in $matched_dirs; do
-  echo Adding files from directory $d to PATH...
+  echo "Adding files from directory $d to PATH..."
   PATH=$PATH:$d
 done
 
 echo "Searching for .source-this files and sourcing them..."
 matched_dirs=$(find $HOME/workplace-specific/ -name .source-this -printf "%h\n")
 for d in $matched_dirs; do
-  echo Sourcing files from $d
+  echo "Sourcing files from $d"
   for f in $(find $d -maxdepth 1 -iname  "*.sh"); do
-    echo Sourcing file $f
+    echo "Sourcing file $f"
     . "$f"
   done
 done
