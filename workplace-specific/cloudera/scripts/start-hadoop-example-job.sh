2b0264be04791cae6262d33061788363b1307c8d
nemethszyszy <szilard.nemeth88@gmail.com>
2018-01-10 17:39:31 +0100

    add dynamic pom version to start-hadoop-example-job.sh

--- workplace-specific/cloudera/scripts/start-hadoop-example-job.sh
+++ workplace-specific/cloudera/scripts/start-hadoop-example-job.sh
@@ -4,10 +4,16 @@ function start-hadoop-example-job {
     HADOOP_PARAMETERS=$1
     #example parameters: 'pi -Dmapreduce.framework.name=yarn -Dmapreduce.map.resource.gpu=5000m 10 100'
 
-    HADOOP_DIR="$HOME/development/apache/hadoop-maven/"
-    HADOOP_DIST_DIR="$HADOOP_DIR/hadoop-dist/"
-    HADOOP_VERSION="3.1.0-SNAPSHOT"
+    HADOOP_MVN_DIR="$HOME/development/apache/hadoop-maven/"
+    HADOOP_DIST_DIR="$HADOOP_MVN_DIR/hadoop-dist/"
 
+    #update $version with current pom.xml version in mapred-site.xml
+    pushd $HADOOP_MVN_DIR
+    HADOOP_VERSION=$(echo '${project.version}' | mvn help:evaluate 2> /dev/null | grep -v '^[[]')
+    popd
+
+    set -x
     $HADOOP_DIST_DIR/target/hadoop-$HADOOP_VERSION/bin/hadoop jar \
     $HADOOP_DIST_DIR/target/hadoop-$HADOOP_VERSION/share/hadoop/mapreduce/hadoop-mapreduce-examples-$HADOOP_VERSION.jar $1
+    set +x
 }
