4dce7c3aa114a88c1852fbf166353eb441da773d
nemethszyszy <szilard.nemeth88@gmail.com>
2018-02-06 09:04:45 +0100

    add some cloudera aliases

--- workplace-specific/cloudera/aliases/aliases.sh
+++ workplace-specific/cloudera/aliases/aliases.sh
@@ -4,9 +4,13 @@
 CLOUDERA_DEV_ROOT="$HOME/development/cloudera/"
 CLOUDERA_HADOOP_ROOT="$CLOUDERA_DEV_ROOT/hadoop/"
 HADOOP_MVN_DIR="$HOME/development/apache/hadoop-maven/"
+HADOOP_DEV_DIR="$HOME/development/apache/hadoop/"
 
 
 #goto aliases
 alias goto-cldr="cd $CLOUDERA_DEV_ROOT"
 alias goto-cldr-hadoop="cd $CLOUDERA_HADOOP_ROOT"
 alias goto-tasks="cd $HOME/Google Drive File Stream/My Drive/development/tasks/"
+
+#git special commands
+alias git-mybranches="git branch | grep 'own-'"
