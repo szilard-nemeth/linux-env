e2764366dac88e1dd7f22304b9f2a72fced39871
nemethszyszy <szilard.nemeth88@gmail.com>
2018-10-08 14:10:57 -0700

    add some yarn and git aliases

--- workplace-specific/cloudera/aliases/aliases.sh
+++ workplace-specific/cloudera/aliases/aliases.sh
@@ -14,16 +14,19 @@ alias goto-tasks="cd $HOME/Google Drive File Stream/My Drive/development/tasks/"
 alias goto-hadoop="cd $HADOOP_DEV_DIR"
 alias goto-hadoop-mvn="cd $HADOOP_MVN_DIR"
 
-#git special commands
+#git specific commands
 alias git-mybranches="git branch | grep 'own-'"
 alias gerrit-branches5="git br -r | grep gerrit | grep -e '5.1\d.*' | cut -d_ -f 2-3 | sort -u | grep -v patch"
 alias git-remove-trailing-ws="git diff-tree --no-commit-id --name-only -r HEAD | xargs sed -i 's/[[:space:]]*$//'"
+alias git-rebase-trunk="git co trunk && echo 'Pulling origin/trunk...' && git pull && git co - && git rebase trunk"
+
 alias j7='export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_80.jdk/Contents/Home'
 alias j8='export JAVA_HOME=/Library/Java//JavaVirtualMachines/jdk1.8.0_151.jdk/Contents/Home'
 
 #YARN-related commands
-alias yarn-upstream-gergo="git log --grep grepas --grep 'Gergo Repas' --oneline  | wc -l"
-alias yarn-upstream-own="git log --grep snemeth --grep 'Szilard Nemeth' --oneline | wc -l"
+alias upstream-gergo="git log --grep grepas --grep 'Gergo Repas' --oneline  | wc -l"
+alias upstream-yarn="git log --grep snemeth --grep Szilard --grep 'Szilard Nemeth' --oneline | grep YARN | wc -l"
+alias upstream-hadoop="git log --grep snemeth --grep Szilard --grep 'Szilard Nemeth' --oneline | wc -l"
 
 alias grind-yarn="export GRIND_MAVEN_FLAGS=-Dmaven-dependency-plugin.version=2.10 && \
 rm -rf ~/development/cloudera/dist_test/env/.grind/cache/ && \
