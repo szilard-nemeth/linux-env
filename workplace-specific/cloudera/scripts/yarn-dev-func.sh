d5f4ed9b78b10a753c3f634724d778ddfd1f72b6
nemethszyszy <szilard.nemeth88@gmail.com>
2018-11-10 15:42:45 +0100

    add yarn-backport function

--- workplace-specific/cloudera/scripts/yarn-dev-func.sh
+++ workplace-specific/cloudera/scripts/yarn-dev-func.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ##TODO add force mode: ignore whitespace issues and make backup of patch!
-function save-patch() {
+function yarn-save-patch() {
     BRANCH_NAME=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
     if [ "$BRANCH_NAME" = "trunk" ]; then
         echo "Cannot make patch, current branch is trunk!"
@@ -117,5 +117,65 @@ function yarn-create-review-branch() {
         git apply $PATCH_FILE
         git commit -am "patch file: $PATCH_FILE"
     fi
+}
+
+#TODO decide on the cdh branch whether this is C5 or C6 backport (remote is different)
+function yarn-backport-c6() {
+    if [[ $# -ne 3 ]]; then
+        echo "Usage: yarn-backport-c6 [CDH-jira-number] [CDH-branch] [Upstream commit hash or commit message fragment]"
+        echo "Example: yarn-backport-c6 CDH-64201 cdh6.x YARN-7948"
+        return 1
+    fi
+    CDH_JIRA_NO=$1
+    CDH_BRANCH=$2
+    UPSTREAM_PATCH_NO=$3
     
+    ##fetch, pull, store commit hash of upstream commit
+    goto-hadoop
+    ORIG_BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+    git fetch --all && git co trunk && git pull
+    
+    IFS=$'\n'
+    GIT_LOG_RES=($(git log --oneline --grep=${UPSTREAM_PATCH_NO}))
+    unset IFS
+    echo "git log res: ${GIT_LOG_RES[@]}"
+    if [[ ${#GIT_LOG_RES[@]} -ne 1 ]]; then 
+        echo "Multiple results found in git log of upstream repository for pattern: $UPSTREAM_PATCH_NO";
+        
+        #restore original upstream branch
+        git co ${ORIG_BRANCH}
+        return 1 
+    fi
+    
+    UPSTREAM_COMMIT_HASH=$(echo ${GIT_LOG_RES} | cut -d' ' -f1)
+    #restore original upstream branch
+    git co ${ORIG_BRANCH}
+    
+    
+    ###do the rest of the work in the cloudera repo
+    goto-cldr-hadoop
+    git fetch --all 
+    git co -b "$CDH_JIRA_NO-$CDH_BRANCH" cauldron/$CDH_BRANCH
+    git cherry-pick -x $UPSTREAM_COMMIT_HASH
+    
+    if [ $? -ne 0 ]; then
+        echo "!!!There was merge conflicts, please resolve them!!!"
+        return 1
+    fi
+    
+    #TODO check result of cherry-pick (could be non-zero if we have merge conflicts)
+    ##add CDH number and it will add gerrit Change-Id
+    OLD_MSG=$(git log --format=%B -n1)
+    git commit --amend -m"$CDH_JIRA_NO: $OLD_MSG"
+    
+    
+    ##run build to verify backport compiles fine
+    #TODO make an option that decides if mvn clean install should be run!
+    #mvn clean install -Pdist -DskipTests -Pnoshade  -Dmaven.javadoc.skip=true
+    
+    
+    ##push to gerrit
+    echo "Commit was successful! Run this command to push to gerrit: git push cauldron HEAD:refs/for/$CDH_BRANCH"
+    #git push cauldron HEAD:refs/for/$CDH_BRANCH
+
 }
