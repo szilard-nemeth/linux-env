ebea204afa4a4a587f9b02a7fb789e3399495274
nemethszyszy <szilard.nemeth88@gmail.com>
2019-03-16 18:29:46 +0100

    Add git-operations.sh

---
+++ workplace-specific/cloudera/scripts/git-operations.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+function git-search-branches() {
+    for sha1 in `git log --oneline --all --grep "$1" | cut -d" " -f1`
+    do
+        git branch -r --contains ${sha1}
+    done
+}
+function git-search-tags() {
+    for sha1 in `git log --oneline --all --grep "$1" | cut -d" " -f1`
+    do
+        git tag --contains ${sha1}
+    done
+}
+
