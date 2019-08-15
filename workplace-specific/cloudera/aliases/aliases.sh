21dfc80b6e5d393912e50cc31bbfd6d999047255
nemethszyszy <szilard.nemeth88@gmail.com>
2019-08-15 17:39:46 +0200

    add cloudera aliases

--- workplace-specific/cloudera/aliases/aliases.sh
+++ workplace-specific/cloudera/aliases/aliases.sh
@@ -13,6 +13,7 @@ alias goto-cldr-hadoop="cd $CLOUDERA_HADOOP_ROOT"
 alias goto-tasks="cd $HOME/Google Drive File Stream/My Drive/development/tasks/"
 alias goto-hadoop="cd $HADOOP_DEV_DIR"
 alias goto-hadoop-mvn="cd $HADOOP_MVN_DIR"
+alias goto-hadoop-commit="cd $HOME/development/apache/hadoop-commit"
 alias goto-systest="cd $CLOUDERA_DEV_ROOT/qe-cmf/systest"
 alias goto-cmf="cd $CLOUDERA_DEV_ROOT/cmf"
 alias goto-yarn-tasks="cd $HOME/yarn-tasks"
@@ -30,11 +31,14 @@ alias j8='export JAVA_HOME=/Library/Java//JavaVirtualMachines/jdk1.8.0_151.jdk/C
 #YARN-related commands
 alias upstream-gergo="git log --grep grepas --grep 'Gergo Repas' --oneline  | wc -l"
 
+alias upstream-all-count="git log --grep snemeth --grep Szilard --grep 'Szilard Nemeth' --oneline | grep 'YARN\|SUBMARINE\|HADOOP' | wc -l"
+alias upstream-all="git log --grep snemeth --grep Szilard --grep 'Szilard Nemeth' --oneline | grep 'YARN\|SUBMARINE\|HADOOP'"
+alias upstream-hadoop="git log --grep snemeth --grep Szilard --grep 'Szilard Nemeth' --oneline | wc -l"
+alias upstream-committed="git log --author="snemeth@apache.org" --oneline | wc -l"
+
 #TODO write a script: intersection of grep and author commands should give the final number
-alias upstream-yarn="git log --grep snemeth --grep Szilard --grep 'Szilard Nemeth' --oneline | grep YARN | wc -l"
 alias upstream-yarn2="echo $(($(git log --grep snemeth --grep Szilard --grep 'Szilard Nemeth' --oneline | grep YARN | wc -l | tr -s ' ' | cut -d ' ' -f2) + $(git log --author=snemeth --oneline | wc -l | tr -s ' ' | cut -d ' ' -f2)))"
 
-alias upstream-hadoop="git log --grep snemeth --grep Szilard --grep 'Szilard Nemeth' --oneline | wc -l"
 
 alias grind-yarn="export GRIND_MAVEN_FLAGS=-Dmaven-dependency-plugin.version=2.10 && \
 rm -rf ~/development/cloudera/dist_test/env/.grind/cache/ && \
@@ -53,3 +57,5 @@ alias grind-yarn-exceptions="grind --verbose test --java-version 8 \
  -e TestHBaseTimelineStorageSchema"
 
 alias run-findbugs="mvn clean install -DskipTests && mvn findbugs:findbugs && mvn findbugs:gui"
+
+alias mvn-hadoop-patch="mvn -Ptest-patch clean site site:stage"
