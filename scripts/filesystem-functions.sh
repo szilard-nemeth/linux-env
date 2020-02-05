a986251350439db553db9b4fd6c6b0df02abe79f
nemethszyszy <szilard.nemeth88@gmail.com>
2020-02-05 10:38:35 +0100

    Removed comment

--- scripts/filesystem-functions.sh
+++ scripts/filesystem-functions.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-#TODO remove
-#MOUNT the given image parameter
-
-#[root@szyszyLAPTOP _SCRIPTS]# sudo ./mount_ISO.sh "/media/Szyszy's Ext_HDD/torrents/diablo_film/Dr House 1.évad/House_1-1.ISO"
-#total 4
-#dr-xr-xr-x 2 4294967295 4294967295   40 2006-05-31 21:41 AUDIO_TS
-#dr-xr-xr-x 2 4294967295 4294967295 1444 2006-05-31 21:41 VIDEO_TS
- 
-
 function mount-iso() {
 #    dir=/home/szyszy/mnt/temp_imagedisk
     dir=$1
