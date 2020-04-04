f78ac19366efd05f0b6d1b096ee27f7c3f1ee5c0
nemethszyszy <szilard.nemeth88@gmail.com>
2020-04-04 11:06:31 +0200

    Move git scripts from CLDR to common

--- scripts/git.sh
+++ scripts/git.sh
@@ -6,3 +6,16 @@ function commit-all-unstaged() {
     done
     git commit
 }
+
+function git-search-branches() {
+    for sha1 in `git log --oneline --all --grep "$1" | cut -d" " -f1`
+    do
+        git branch -r --contains ${sha1}
+    done
+}
+function git-search-tags() {
+    for sha1 in `git log --oneline --all --grep "$1" | cut -d" " -f1`
+    do
+        git tag --contains ${sha1}
+    done
+}
