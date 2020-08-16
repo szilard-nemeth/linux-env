18825398010b4d3587010074732549bdb12a4b25
nemethszyszy <szilard.nemeth88@gmail.com>
2020-08-16 09:14:22 +0200

    Print object str with auto_str + auto_repl

--- workplace-specific/cloudera/scripts/yarn/python/utils.py
+++ workplace-specific/cloudera/scripts/yarn/python/utils.py
@@ -10,14 +10,28 @@ REVIEW_BRANCH_SEP = '-'
 LOG = logging.getLogger(__name__)
 
 
-def auto_str(cls):
+def auto_str(cls, with_repr=True):
     def __str__(self):
         return '%s(%s)' % (
             type(self).__name__,
             ', '.join('%s=%s' % item for item in vars(self).items())
         )
 
+    def __repr__(self):
+        return __str__(self)
+
     cls.__str__ = __str__
+
+    if with_repr:
+        cls.__repr__ = __repr__
+    return cls
+
+
+def auto_repr(cls):
+    def __repr__(self):
+        return self.__str__()
+
+    cls.__repr__ = __repr__
     return cls
 
 
