9ee2a16f5a2da86f96d5d8c49c01527ace3d9434
nemethszyszy <szilard.nemeth88@gmail.com>
2020-12-22 12:42:16 +0100

    Cloudera: Investigation/YARN-10427: Make scripts ready to execute upstream Hadoop build as well

--- workplace-specific/cloudera/investigations/YARN-10427/scripts/setup-vars.sh
+++ workplace-specific/cloudera/investigations/YARN-10427/scripts/setup-vars.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-HADOOP_BUILD_ROOT="/Users/szilardnemeth/development/cloudera/hadoop/"
-INVESTIGATION_BASEDIR="$LINUX_ENV_REPO/workplace-specific/cloudera/investigations/YARN-10427"
-HADOOP_VERSION=$(cd $HADOOP_BUILD_ROOT && echo '${project.version}' | mvn help:evaluate 2> /dev/null | grep -v '^[[]')
-MVN_BUILD_COMMAND="mvn clean package -s $HOME/.m2/settings-cdpd.xml -Pdist -DskipTests -Dmaven.javadoc.skip=true"
+#echo "Setting up vars for downstream Hadoop..."
+#. ./setup-vars-downstream.sh
 
-#REMOTE-HOST SPECIFIC
-CLUSTER_HOST1="root@snemeth-fips2-1.vpc.cloudera.com"
-REMOTE_BASEDIR="/root/YARN-10427"
+echo "Setting up vars for upstream Hadoop..."
+. ./setup-vars-upstream.sh
