489dd9b4af158d166e7c794b800352b0de135e58
nemethszyszy <szilard.nemeth88@gmail.com>
2021-04-11 00:37:05 +0200

    Removed legacy git_compare.sh

--- workplace-specific/cloudera/scripts/git/git_compare.sh
+++
@@ -1,9 +0,0 @@
-result=`git log $1..$2 --oneline --no-merges | grep -e '[A-Z]\+-[0-9]\+' -o`
-gitlog=`git log $1 --oneline --no-merges`
-
-for value in $result
-do
-  if ! grep -q "$value" <<< "$gitlog"; then
-    echo "$value exists on branch=$2, but not exist on $1"
-  fi
-done
