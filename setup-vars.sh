481ad3e1309f54ee655efac226d903c2370f9408
nemethszyszy <szilard.nemeth88@gmail.com>
2022-09-25 17:29:46 +0200

    setup-vars.sh: Reorganize file

--- setup-vars.sh
+++ setup-vars.sh
@@ -1,29 +1,25 @@
 #!/usr/bin/env bash
 echo "Loading setup-vars.sh..."
 
-export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=1024m"
-export ANT_OPTS="-XX:PermSize=512m -XX:MaxPermSize=512m -Xmx1024m -Xms1024m"
-export GIT_EDITOR='vim'
-
 #===================================
 #Setup npm
 export NODE_PATH='/usr/local/lib/node_modules'
 NPM_PACKAGES="${HOME}/.npm-packages"
 PATH="$NPM_PACKAGES/bin:$PATH"
+# Unset manpath so we can inherit from /etc/manpath via the `manpath` command
+unset MANPATH # delete if you already modified MANPATH elsewhere in your config
+export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
+#End of npm config
 #===================================
 
-#===================================
 #Setup Ruby
 PATH="$HOME/.rbenv/shims:$PATH"
 PATH=$(find $(gem environment gempath | tr ':' '\n') -type d -name bin | tr '\n' ':'):$PATH 
 #===================================
 
-
-# Unset manpath so we can inherit from /etc/manpath via the `manpath` command
-unset MANPATH # delete if you already modified MANPATH elsewhere in your config
-export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
-#End of npm config
-
+export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=1024m"
+export ANT_OPTS="-XX:PermSize=512m -XX:MaxPermSize=512m -Xmx1024m -Xms1024m"
+export GIT_EDITOR='vim'
 export MY_REPOS_DIR="$HOME/development/my-repos/"
 export OTHER_REPOS_DIR="$HOME/development/other-repos/"
 export FORKED_REPOS_DIR="$HOME/development/my-repos/fork"
