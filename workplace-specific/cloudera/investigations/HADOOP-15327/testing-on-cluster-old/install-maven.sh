9f255d76c906ee6aad7392846709b8e1a6a5ac74
nemethszyszy <szilard.nemeth88@gmail.com>
2022-12-01 22:52:56 +0100

    Cloudera: Removed HADOOP-15327 investigation results

--- workplace-specific/cloudera/investigations/HADOOP-15327/testing-on-cluster-old/install-maven.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-wget http://www.eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
-tar xzf apache-maven-3.3.9-bin.tar.gz
-mkdir /usr/local/maven
-mv apache-maven-3.3.9/ /usr/local/maven/
-alternatives --install /usr/bin/mvn mvn /usr/local/maven/apache-maven-3.3.9/bin/mvn 1
-alternatives --config mvn
