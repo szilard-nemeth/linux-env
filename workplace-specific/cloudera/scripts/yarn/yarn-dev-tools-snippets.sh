ba83192b6fd4adc103aa7c8e67ee70ca8ff8a16d
nemethszyszy <szilard.nemeth88@gmail.com>
2022-12-06 09:38:22 +0100

    Cloudera: Add new functions to yarn-dev-tools-snippets.sh

--- workplace-specific/cloudera/scripts/yarn/yarn-dev-tools-snippets.sh
+++ workplace-specific/cloudera/scripts/yarn/yarn-dev-tools-snippets.sh
@@ -7,13 +7,43 @@ function setup() {
 }
 
 #####LESS COMPLEX SCRIPTS CAN STAY HERE, AS IT'S NOT WORTH IT TO MIGRATE THESE SMALL SCRIPTS TO PYTHON
-function get-remote-cm-nm-dir() {
+function yarn-get-remote-cm-nm-dir() {
     #Example usage: get-remote-cm-nm-dir bsteinbach-gpu-2.vpc.cloudera.com
     local host="$1"
     local process_dir="/var/run/cloudera-scm-agent/process/"
     local get_nm_dir_cmd='nm_dir=$(sudo ls -t /var/run/cloudera-scm-agent/process/| grep -m 1 yarn-NODEMANAGER);'"echo $process_dir/"'$nm_dir'
     ssh "systest@${host}" "set -x;$get_nm_dir_cmd;sudo ls -latr $process_dir/"'$nm_dir'
 }
+
+function yarn-get-remote-cm-nm-dir-public-cloud() {
+    #Example usage: get-remote-cm-nm-dir bsteinbach-gpu-2.vpc.cloudera.com
+    local host="$1"
+    local process_dir="/var/run/cloudera-scm-agent/process/"
+    local get_nm_dir_cmd='nm_dir=$(sudo ls -t /var/run/cloudera-scm-agent/process/| grep -m 1 yarn-NODEMANAGER);'"echo $process_dir/"'$nm_dir'
+    ssh -i ~/.ssh/hw-priv-pem-key "cloudbreak@${host}" "set -x;$get_nm_dir_cmd;sudo ls -latr $process_dir/"'$nm_dir'
+}
+
+function yarn-get-remote-cm-rm-dir-public-cloud() {
+    #Example usage: get-remote-cm-nm-dir bsteinbach-gpu-2.vpc.cloudera.com
+    local host="$1"
+    local process_dir="/var/run/cloudera-scm-agent/process/"
+    local get_rm_dir_cmd='rm_dir=$(sudo ls -t /var/run/cloudera-scm-agent/process/| grep -m 1 yarn-RESOURCEMANAGER);'"echo $process_dir/"'$rm_dir'
+    ssh -i ~/.ssh/hw-priv-pem-key "cloudbreak@${host}" "set -x;$get_rm_dir_cmd;sudo ls -latr $process_dir/"'$rm_dir'
+}
+
+function yarn-fetch-rm-config-from-host-public-cloud {
+    local host="$1"
+    local conf_file="$2"
+    local process_dir="/var/run/cloudera-scm-agent/process/"
+    local get_rm_dir_cmd='rm_dir=$(sudo ls -t /var/run/cloudera-scm-agent/process/| grep -m 1 yarn-RESOURCEMANAGER);'"echo $process_dir/"'$rm_dir'
+    #ssh -i ~/.ssh/hw-priv-pem-key "cloudbreak@${host}" "set -x;$get_rm_dir_cmd;sudo cp $process_dir/"'$rm_dir'"/{nodes_allow.txt,nodes_exclude.txt} ~/"
+    ssh -i ~/.ssh/hw-priv-pem-key "cloudbreak@${host}" "set -x;$get_rm_dir_cmd;sudo cp $process_dir/"'$rm_dir'"/$conf_file ~/;sudo chown cloudbreak ~/$conf_file"
+    set -x
+    echo $host
+    scp -i ~/.ssh/hw-priv-pem-key cloudbreak@${host}:$conf_file "./${host}_${conf_file}"
+    set +x
+}
+
 
 function timezones() {
     echo -n "Local time: " && TZ=CET date; \
