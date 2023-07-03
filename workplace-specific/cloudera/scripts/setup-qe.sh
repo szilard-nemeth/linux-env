43e7ce2907fedf06192e77bb827b521ba673ed23
nemethszyszy <szilard.nemeth88@gmail.com>
2023-07-03 18:24:53 -0400

    minor changes in setup-qe.sh

--- workplace-specific/cloudera/scripts/setup-qe.sh
+++ workplace-specific/cloudera/scripts/setup-qe.sh
@@ -2,8 +2,10 @@
 
 function kubeconf-qe() {
     export KUBECONFIG=$HOME/.kube/ocp-qaas-prod-qe.conf
+    echo "KUBECONFIG=$KUBECONFIG"
 }
 
 function kubeconf-qaas() {
     export KUBECONFIG=$HOME/.kube/rke-qaas-prod-qe.conf
+    echo "KUBECONFIG=$KUBECONFIG"
 }
