8056554467b5fc900b69fb3b5daa1c09af2980a4
nemethszyszy <szilard.nemeth88@gmail.com>
2025-02-12 22:36:23 -0500

    setup-dex-env.sh: Add some new dex-logsearch functions

--- workplace-specific/cloudera/scripts/setup-dex-env.sh
+++ workplace-specific/cloudera/scripts/setup-dex-env.sh
@@ -1477,6 +1477,10 @@ function dex-logsearch-last-hour {
   AWS_PROFILE=cu_logs_dev log-search --env dev --from 'now-1h/h' --to 'now/h' '@app:dex'
 }
 
+function dex-logsearch-last-hour2 {
+  AWS_PROFILE=cu_logs_dev log-search --env dev --from 'now-1h/h' --to 'now/h'  --fields-to-print "@timestamp,@message" --print-stats --print-config "@app.keyword:dex AND @cluster:mow-priv AND @env:manowar_dev"
+}
+
 function dex-logsearch-dex-13563 {
   # FROM: May 9 2024 9:18:00 AM 
   # TO: May 9 2024, 5:12:00 PM --> Add 30 seconds
@@ -1493,6 +1497,14 @@ function dex-logsearch-dex-14173 {
   echo "Saving results to file: $dest_file"
   set -x
   AWS_PROFILE=cu_logs_dev log-search --env dev --from "2024-07-16T17:01:00.000Z" --to "2024-07-16T17:49:00.000Z" --fields-to-print "@timestamp,@message" --print-stats --print-config "@app.keyword:dex AND @cluster:mow-int-eu-central-1 AND @env:manowar_int_euc1" > $dest_file
+  set +x
+}
+
+function dex-logsearch-ENGESC-26999-prod {
+  local dest_file=/tmp/logsearch-results-ENGESC-26999-$(date-formatted)
+  echo "Saving results to file: $dest_file"
+  set -x
+  AWS_PROFILE=cu_logs_prod log-search --env prod --from "2024-09-25T16:26:39.000Z" --to "2024-09-25T16:27:42.000Z" --fields-to-print "@timestamp,@message" --print-stats --print-config "@app.keyword:dex AND @cluster:mow-prod AND @env:manowar_prod" > $dest_file
   set +x
 }
 
