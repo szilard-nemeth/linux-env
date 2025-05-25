3ec3e482e42a8f4cd38c9ab4f3620b2b5b687732
nemethszyszy <szilard.nemeth88@gmail.com>
2025-05-25 15:46:15 -0400

    generic.sh: Add trace_to_file

--- scripts/generic.sh
+++ scripts/generic.sh
@@ -12,6 +12,53 @@ function debug-command {
     set +x;
 }
 
+function trace_to_file {
+    if (( $# < 2 )); then
+        echo "Usage: trace_to_file logfile command [args...]"
+        echo "Usage: trace_to_file mytrace.log bash -c '
+            echo "Running multiple commands"
+            ls -l /nonexistent
+        '"  
+    return 1
+    fi
+
+    local logfile=$1
+    shift
+
+
+    # Open FD 3 for writing to the logfile
+    exec 3>"$logfile"
+
+    # Naive approach, prints: 
+    # +trace_to_file:19> echo faf
+    # +trace_to_file:20> set +x
+    # {
+    #     set -x
+    #     "$@"
+    #     set +x
+    # } 2>&3
+
+
+    # Quote and join arguments safely
+    local cmd_str=""
+    for arg in "$@"; do
+        cmd_str+=" $(printf '%q' "$arg")"
+    done
+
+    # Set PS4 to include a timestamp
+    local ps4='+$(date "+%Y-%m-%d %H:%M:%S") '
+    {
+        PS4="$ps4"
+        set -x
+        eval "$cmd_str"
+        set +x
+    } 2>&3
+
+    # Close FD 3
+    exec 3>&-
+}
+
+
 function show-funcdef {
   declare -f $1
 }
