b057680f0beca4cd5c28fe1ffc77d304eeabd7fa
nemethszyszy <szilard.nemeth88@gmail.com>
2025-05-30 21:35:36 -0400

    Cloudera / compare_catalog.py: Add logging

--- workplace-specific/cloudera/scripts/cde/runtime-catalog/compare_catalog.py
+++ workplace-specific/cloudera/scripts/cde/runtime-catalog/compare_catalog.py
@@ -1,6 +1,21 @@
+#!/usr/bin/python3
+
 import json
+import logging
 import sys
 import os
+
+DEBUG_ENABLED = True
+LOG_FILE = '/tmp/compare-catalog.log'
+
+def configure_logging(debug=False):
+    level = logging.DEBUG if debug else logging.INFO
+    fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+
+    logging.basicConfig(filename=LOG_FILE,
+                        filemode="w",
+                        format=fmt,
+                        level=level)
 
 def load_json(filename):
     with open(filename, 'r') as f:
@@ -46,60 +61,72 @@ def compare_mapped_json(map1, map2, entry_to_id_map1, entry_to_id_map2):
         val1 = map1.get(key)
         val2 = map2.get(key)
 
-        print(f"[{idx}] --- Comparing key: {key} ---")
+        _print(f"[{idx}] --- Comparing key: {key} ---")
         if val1 and val2:
             diffs = compare_dicts(val1, val2)
             if not diffs:
-                print("MATCH")
+                _print("MATCH")
             else:
-                print("DIFFERENCES FOUND:")
+                _print("DIFFERENCES FOUND:")
 
                 # Print catalog ids first
                 old_catalog_id = {entry_to_id_map1[key]}
                 new_catalog_id = {entry_to_id_map2[key]}
                 if old_catalog_id != new_catalog_id:
-                    print(f"  old catalog id: {entry_to_id_map1[key]}")
-                    print(f"  new catalog id: {entry_to_id_map2[key]}")
+                    _print(f"  old catalog id: {entry_to_id_map1[key]}")
+                    _print(f"  new catalog id: {entry_to_id_map2[key]}")
                 else:
-                    print(f"  catalog id: {old_catalog_id}")
+                    _print(f"  catalog id: {old_catalog_id}")
 
                 for field, v1, v2 in diffs:
                     #Don't print differences in these fields as this will be changing in all the entries and too much verbose
                     if  any(val in field for val in ["digest", "id", "attr.sparkVersion", ".tag"]):
                         pass
                     else :
-                        print(f"Field: {field}")
-                        print(f"  old: {v1}")
-                        print(f"  new: {v2}")
+                        _print(f"Field: {field}")
+                        _print(f"  old: {v1}")
+                        _print(f"  new: {v2}")
         elif val1:
-            print("Only in File1")
+            _print("Only in File1")
         elif val2:
-            print("Only in File2")
-        print()
+            _print("Only in File2")
+        _print("")
 
 
 def check_args():
     if len(sys.argv) != 3:
-        print("Error: Exactly 2 parameters are required.")
-        print("Usage: python script.py <old catalog> <new catalog>")
+        _print("Error: Exactly 2 parameters are required.")
+        _print("Usage: python script.py <old catalog> <new catalog>")
         sys.exit(1)
 
     file1, file2 = sys.argv[1], sys.argv[2]
 
     for file in (file1, file2):
         if not os.path.isfile(file):
-            print(f"Error: '{file}' is not a valid file.")
+            _print(f"Error: '{file}' is not a valid file.")
             sys.exit(1)
 
     # print("Both files are valid.")
     return file1, file2
 
+def _print(msg, debug=False):
+    print(msg)
+
+    if debug:
+        logging.getLogger().debug(msg)
+    else:
+        logging.getLogger().info(msg)
+
 if __name__ == "__main__":
     # old_data = load_json('/Users/snemeth/Downloads/catalog-entries.json') # File1
     # new_data = load_json('/Users/snemeth/Downloads/catalog-entries-dale.json') # File2
+    configure_logging(debug=DEBUG_ENABLED)
+
+
     old_catalog, new_catalog = check_args()
-    print(f"Old catalog file: {old_catalog} (File 1)")
-    print(f"New catalog file: {new_catalog} (File 2)")
+    _print(f"Old catalog file: {old_catalog} (File 1)")
+    _print(f"New catalog file: {new_catalog} (File 2)")
+    _print(f"Logging to file: {LOG_FILE}")
 
     old_data = load_json(old_catalog) # File1
     new_data = load_json(new_catalog) # File2
