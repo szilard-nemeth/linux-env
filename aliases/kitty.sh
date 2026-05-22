4ac7d04f038376eed73ecd636de6cf4e60e05b17
nemethszyszy <szilard.nemeth88@gmail.com>
2026-05-22 17:39:24 -0400

    Add Kitty aliases

---
+++ aliases/kitty.sh
@@ -0,0 +1,3 @@
+alias kitty-copy-screen="kitty @ get-text --extent screen | kitty +kitten clipboard"
+alias kitty-copy-last-command="kitty @ get-text --extent last_cmd_output | kitty +kitten clipboard"
+alias kitty-copy-all="kitty @ get-text --extent all | kitty +kitten clipboard"
